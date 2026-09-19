f7d66108e5cf9d75704978d2edbde4238025e4f0
mickr777 <115216705+mickr777@users.noreply.github.com>
2026-09-19 22:00:10 +1000

    Clarify remote mirror transport description
    Updated the module description to clarify the transport type.

--- invokeai/app/invocations/remote_worker/__init__.py
+++ invokeai/app/invocations/remote_worker/__init__.py
@@ -1,4 +1,4 @@
-"""Built-in Windows-to-InvokeAI remote mirror transport.
+"""Built-in InvokeAI-to-InvokeAI remote mirror transport.
 
 The invocation is discovered by InvokeAI core module discovery; installing a
 standalone custom-node pack is not required.
